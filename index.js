fa2069021f93a20ca26cb0b65344549c153597b8
Tony <tonylehuynh66@gmail.com>
2023-06-11 12:51:16 +1000

    updateScore function complete

--- index.js
+++ index.js
@@ -173,5 +173,7 @@ function changeDirection(event){
 			break;
 	}
 };
-function updateScore(){};
+function updateScore(){
+	scoreText.textContent = `${player1Score} : ${player2Score}`;
+};
 function resetGame(){};
