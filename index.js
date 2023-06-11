05d4a3191376874a32934d82e6d3fa0d7bf53279
Tony <tonylehuynh66@gmail.com>
2023-06-11 12:33:35 +1000

    create Ball function complete

--- index.js
+++ index.js
@@ -74,7 +74,26 @@ function drawPaddles(){
 	context.strokeRect(paddle2.x, paddle2.y, paddle2.width, paddle2.height);
 };
 
-function createBall(){};
+function createBall(){
+	ballSpeed = 1;
+	// If number is 1 move to right/down, if 0 then move left/up
+	if(Math.round(Math.random()) == 1){
+		ballXDirection = 1;
+	}
+	else{
+		ballXDirection = -1;
+	}
+	if(Math.round(Math.random()) == 1){
+		ballYDirection = 1;
+	}
+	else{
+		ballYDirection = -1;
+	}
+	// Start ball in center
+	ballX = gameWidth / 2;
+	ballY = gameHeight / 2;
+	drawBall(ballX, ballY);
+};
 function moveBall(){};
 function drawBall(ballX, ballY){
 	  context.fillStyle = ballColor;
